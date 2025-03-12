6e12b414f3010873ae9c1d5146d72e3ee0d196fb
michelangelomancini <michelangelomancini8@gmail.com>
2025-03-12 14:16:44 +0100

    consegna MILESTONE 3

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,24 +1,57 @@
-// importo MovieCard per creare lista dei film
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+// importo il component MovieCard per la creazione lista dei film
 import MovieCard from "../components/MovieCard"
 
-// creo funzione che mi ritorni lista film
-const HomePageFilms = () => {
+// creo funzione che mi ritorni lista film nella homepage
+const HomePageMovies = () => {
+
+
+    // destructuring variabili di stato
+    const [movies, setMovies] = useState([]);
+
+    // effettuo la chiamata  API al backend per ottenere i dati
+    function fetchMovies() {
+        axios
+            .get("http://localhost:5000/movies")
+
+            // In caso di successo aggiorno lo stato movies con i dati aggiornati
+            .then((res) => {
+                console.log("Risposta ricevuta dal server:", res.data);
+                setMovies(res.data);
+            })
+
+            // In caso di errore stampo in console un messaggio
+            .catch((err) => console.error("Errore nel recupero dei post:", err));
+    }
+
+    // evito il loop della funzione
+    useEffect(fetchMovies, []);
+
+    // funzione di rendering delle card dei film
+    const renderMovies = () => {
+
+        // mappo movies per generare la card con i dati del film
+        return movies?.map(
+            movie => {
+                return (
+
+                    <MovieCard key={movie.id} />
+                )
+            }
+        )
+    }
+
     return (
         <>
             <h1 className="homelist-title">Lista film preferiti della community!</h1>
             <div className="movies-container">
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
-                <MovieCard />
+                {renderMovies()}
             </div>
 
         </>
     )
 }
 
-export default HomePageFilms
+export default HomePageMovies
